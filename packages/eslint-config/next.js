1dfedf4f4f18f61b27978fd4d3168bc5c780d7f5
Rahul Shetty <shettyrahul8june@gmail.com>
2025-02-07 02:27:54 +0530

    feat: refactor ESLint config for Next.js, move React rules to separate file

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,11 +1,9 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginReact from "eslint-plugin-react";
-import globals from "globals";
 import pluginNext from "@next/eslint-plugin-next";
 import { config as baseConfig } from "./base.js";
+import { config as reactInternalConfig } from "./react-internal.js";
 
 /**
  * A custom ESLint configuration for libraries that use Next.js.
@@ -17,15 +15,7 @@ export const nextJsConfig = [
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
-  {
-    ...pluginReact.configs.flat.recommended,
-    languageOptions: {
-      ...pluginReact.configs.flat.recommended.languageOptions,
-      globals: {
-        ...globals.serviceworker,
-      },
-    },
-  },
+  ...reactInternalConfig,
   {
     plugins: {
       "@next/next": pluginNext,
@@ -33,17 +23,6 @@ export const nextJsConfig = [
     rules: {
       ...pluginNext.configs.recommended.rules,
       ...pluginNext.configs["core-web-vitals"].rules,
-    },
-  },
-  {
-    plugins: {
-      "react-hooks": pluginReactHooks,
-    },
-    settings: { react: { version: "detect" } },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
-      "react/react-in-jsx-scope": "off",
     },
   },
 ];

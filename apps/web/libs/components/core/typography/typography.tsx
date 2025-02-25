10d9175a132d35521b189b38c332b2a67b57f26a
Rahul Shetty <shettyrahul8june@gmail.com>
2025-02-25 13:09:23 +0530

    feat(config): add tailwindcss-animate dependency

---
+++ apps/web/libs/components/core/typography/typography.tsx
@@ -0,0 +1,20 @@
+import { FC, HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+export const H1: FC<HTMLAttributes<HTMLHeadingElement>> = ({
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <h1
+      className={twMerge(
+        "font-[family-name:var(--font-avenir-heavy)] text-3xl sm:text-5xl",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </h1>
+  );
+};
--- packages/tools/tailwind-config/tailwind.config.ts
+++ packages/tools/tailwind-config/tailwind.config.ts
@@ -3,10 +3,12 @@ import type { Config } from "tailwindcss";
 // We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = {
   theme: {
-    extend: {
-      backgroundImage: {
-        "glow-conic":
-          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+    colors: {
+      gold: {
+        300: "#E09F05",
+      },
+      sand: {
+        300: "#C9AD8B", // beige
       },
     },
   },

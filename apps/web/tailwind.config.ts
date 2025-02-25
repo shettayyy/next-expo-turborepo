8b333fad5c437d6ec6998c18ea031841068f44a7
Rahul Shetty <shettyrahul8june@gmail.com>
2025-02-25 11:51:34 +0530

    feat(config): expand tailwind css content paths to include component

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,7 +4,11 @@ import sharedConfig from "@edenomics-tools/tailwind-config";
 import type { Config } from "tailwindcss";
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx"],
+  content: [
+    "./app/**/*.tsx",
+    "./libs/components/**/*.tsx",
+    "./libs/screens/**/*.tsx",
+  ],
   presets: [sharedConfig],
 };
 
